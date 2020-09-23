7a41805e02e73862e7fd5c97ce3a852470ec4957
Harry Mallon <hjmallon@gmail.com>
2020-09-23 14:05:23 +0100

    Update 'crc32c' to 1.1.1 (#256)

--- cmake/projects/crc32c/hunter.cmake
+++ cmake/projects/crc32c/hunter.cmake
@@ -12,6 +12,12 @@ hunter_add_version(
     URL "https://github.com/hunter-packages/crc32c/archive/hunter-1.0.5.tar.gz"
     SHA1 "dc7fa8cb877b9cb12a4e22e553087fdd6ce28cce")
 
+hunter_add_version(
+    PACKAGE_NAME crc32c
+    VERSION "1.1.1"
+    URL "https://github.com/google/crc32c/archive/1.1.1.tar.gz"
+    SHA1 "46f18ed84a12c49c42dc77fff54491a9f8b9dce8")
+
 hunter_cmake_args(crc32c CMAKE_ARGS
     CRC32C_BUILD_TESTS=OFF
     CRC32C_BUILD_BENCHMARKS=OFF
