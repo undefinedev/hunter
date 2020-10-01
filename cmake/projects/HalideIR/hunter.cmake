25b28ec768d9f7714d63e3d184bde260990a0801
Rahul Sheth <rahul@snap.com>
2020-10-01 09:31:45 -0400

    Update project

--- cmake/projects/HalideIR/hunter.cmake
+++ cmake/projects/HalideIR/hunter.cmake
@@ -14,7 +14,7 @@ hunter_add_version(
     VERSION
     0.0-2f3ecdf-p0
     URL
-    "https://github.com/hunter-packages/HalideIR/archive/v0.0-2f3ecdf-p0.tar.gz"
+    "https://github.com/cpp-pm/HalideIR/archive/v0.0-2f3ecdf-p0.tar.gz"
     SHA1
     1f1dfb75eb296ad874aac67cf5b604ddfbe944a1
 )
@@ -25,7 +25,7 @@ hunter_add_version(
     VERSION
     0.0-b257a92-p1
     URL
-    "https://github.com/hunter-packages/HalideIR/archive/v0.0-b257a92-p1.tar.gz"
+    "https://github.com/cpp-pm/HalideIR/archive/v0.0-b257a92-p1.tar.gz"
     SHA1
     3b527b4e9d07e194f2143ce14563f69dc6a4db93
 )
@@ -36,7 +36,7 @@ hunter_add_version(
     VERSION
     0.0-32057b5-p0
     URL
-    "https://github.com/hunter-packages/HalideIR/archive/v0.0-32057b5-p0.tar.gz"
+    "https://github.com/cpp-pm/HalideIR/archive/v0.0-32057b5-p0.tar.gz"
     SHA1
     f1becfb86a2e59e40094a74b200f5d2e374676de
 )
