3b6f94a08925752b7143deb03933e32b611f90bf
Rahul Sheth <rahul@snap.com>
2020-10-01 09:31:26 -0400

    Update project

--- cmake/projects/cctz/hunter.cmake
+++ cmake/projects/cctz/hunter.cmake
@@ -9,7 +9,7 @@ include(hunter_pick_scheme)
 hunter_add_version(
     PACKAGE_NAME cctz
     VERSION "2.2.0"
-    URL "https://github.com/hunter-packages/cctz/archive/hunter-2.2-p0.tar.gz"
+    URL "https://github.com/cpp-pm/cctz/archive/hunter-2.2-p0.tar.gz"
     SHA1 "86d5ab7555a2f6a8c255dcb50fb7734c4cb8fdfd")
 
 hunter_cmake_args(cctz CMAKE_ARGS BUILD_TESTING=OFF BUILD_EXAMPLES=OFF)
