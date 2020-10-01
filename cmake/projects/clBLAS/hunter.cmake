0eb35d0b228fac831937dd20ba01e97422e07b30
Rahul Sheth <rahul@snap.com>
2020-10-01 09:31:28 -0400

    Update project

--- cmake/projects/clBLAS/hunter.cmake
+++ cmake/projects/clBLAS/hunter.cmake
@@ -15,7 +15,7 @@ hunter_add_version(
     VERSION
     2.10.0-p0
     URL
-    "https://github.com/hunter-packages/clBLAS/archive/v2.10.0-p0.tar.gz"
+    "https://github.com/cpp-pm/clBLAS/archive/v2.10.0-p0.tar.gz"
     SHA1
     187cf902c6fa9090a79a38ce7d8fea392077ac7b
 )
