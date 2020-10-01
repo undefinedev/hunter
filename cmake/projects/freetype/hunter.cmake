f6660d4a9a3998d8ef775f4cbeff038cd4a93a24
Rahul Sheth <rahul@snap.com>
2020-10-01 09:31:40 -0400

    Update project

--- cmake/projects/freetype/hunter.cmake
+++ cmake/projects/freetype/hunter.cmake
@@ -25,7 +25,7 @@ hunter_add_version(
     VERSION
     "2.9.1-p1"
     URL
-    "https://github.com/hunter-packages/freetype/archive/v2.9.1-p1.tar.gz"
+    "https://github.com/cpp-pm/freetype/archive/v2.9.1-p1.tar.gz"
     SHA1
     d4000efdea07253ff2dc9132403e09c321ec88ea
 )
@@ -36,7 +36,7 @@ hunter_add_version(
     VERSION
     "2.9.1-p0"
     URL
-    "https://github.com/hunter-packages/freetype/archive/v2.9.1-p0.tar.gz"
+    "https://github.com/cpp-pm/freetype/archive/v2.9.1-p0.tar.gz"
     SHA1
     e3b929a3af402059e88c847dd9b92e92c10401fe
 )
@@ -47,7 +47,7 @@ hunter_add_version(
     VERSION
     "2.6.2"
     URL
-    "https://github.com/hunter-packages/freetype/archive/v2.6.2-p0.tar.gz"
+    "https://github.com/cpp-pm/freetype/archive/v2.6.2-p0.tar.gz"
     SHA1
     d797e30cfa79eb95dc3b67e75fa6d6a3da80d1ae
 )
@@ -58,7 +58,7 @@ hunter_add_version(
     VERSION
     "2.4.12-p0"
     URL
-    "https://github.com/hunter-packages/freetype/archive/v2.4.12-p0.tar.gz"
+    "https://github.com/cpp-pm/freetype/archive/v2.4.12-p0.tar.gz"
     SHA1
     6623f3b91b921c71c4b4485f67b252a7025eb18e
 )
@@ -69,7 +69,7 @@ hunter_add_version(
     VERSION
     "2.4.12-p1"
     URL
-    "https://github.com/hunter-packages/freetype/archive/v2.4.12-p1.tar.gz"
+    "https://github.com/cpp-pm/freetype/archive/v2.4.12-p1.tar.gz"
     SHA1
     7f172287e594c916150f1b2a2882a188f945e969
 )
