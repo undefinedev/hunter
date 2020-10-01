29c483102bc236aceccc44d91a2299794eeddb3d
Rahul Sheth <rahul@snap.com>
2020-10-01 09:31:54 -0400

    Update project

--- cmake/projects/libevhtp/hunter.cmake
+++ cmake/projects/libevhtp/hunter.cmake
@@ -9,7 +9,7 @@ include(hunter_pick_scheme)
 hunter_add_version(
     PACKAGE_NAME libevhtp
     VERSION "1.2.16-p4"
-    URL "https://github.com/hunter-packages/libevhtp/archive/v1.2.16-p4.tar.gz"
+    URL "https://github.com/cpp-pm/libevhtp/archive/v1.2.16-p4.tar.gz"
     SHA1 "c6d7b4b4e27de15670f72e36b127613a707164ee")
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
