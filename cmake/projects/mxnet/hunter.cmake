423954e9af903e0c993e48041113ff2cb4819418
Rahul Sheth <rahul@snap.com>
2020-10-01 09:32:01 -0400

    Update project

--- cmake/projects/mxnet/hunter.cmake
+++ cmake/projects/mxnet/hunter.cmake
@@ -15,7 +15,7 @@ hunter_add_version(
     VERSION
     0.0.0-5b86701f2-p0
     URL
-    "https://github.com/hunter-packages/mxnet/archive/0.0.0-5b86701f2-p0.tar.gz"
+    "https://github.com/cpp-pm/mxnet/archive/0.0.0-5b86701f2-p0.tar.gz"
     SHA1
     8b7d898d2f474bef2ca436cca2c2ab15483535cd
 )
@@ -26,7 +26,7 @@ hunter_add_version(
     VERSION
     0.0.0-5b86701f2-p1
     URL
-    "https://github.com/hunter-packages/mxnet/archive/0.0.0-5b86701f2-p1.tar.gz"
+    "https://github.com/cpp-pm/mxnet/archive/0.0.0-5b86701f2-p1.tar.gz"
     SHA1
     71c01aaa65448f4826e2ec4cadb44987370d664f
 )
@@ -37,7 +37,7 @@ hunter_add_version(
     VERSION
     0.0.0-5b86701f2-p2
     URL
-    "https://github.com/hunter-packages/mxnet/archive/0.0.0-5b86701f2-p2.tar.gz"
+    "https://github.com/cpp-pm/mxnet/archive/0.0.0-5b86701f2-p2.tar.gz"
     SHA1
     60c4d0ef4b9ac35e6a23c5482a1702da7e377d5c
 )
@@ -48,7 +48,7 @@ hunter_add_version(
     VERSION
     1.4.0-p2
     URL
-    "https://github.com/hunter-packages/mxnet/archive/v1.4.0-p2.tar.gz"
+    "https://github.com/cpp-pm/mxnet/archive/v1.4.0-p2.tar.gz"
     SHA1
     bb80e4b173d2630313195f38d528f2d2d42455c3
 )
@@ -59,7 +59,7 @@ hunter_add_version(
     VERSION
     1.4.0-p3
     URL
-    "https://github.com/hunter-packages/mxnet/archive/v1.4.0-p3.tar.gz"
+    "https://github.com/cpp-pm/mxnet/archive/v1.4.0-p3.tar.gz"
     SHA1
     aa01fea9b74bd4a10f5a1d028e61be62c4536bd0
 )
@@ -70,7 +70,7 @@ hunter_add_version(
     VERSION
     1.5.0.rc1-b64e00a-p0
     URL
-    "https://github.com/hunter-packages/mxnet/archive/v1.5.0.rc1-b64e00a-p0.tar.gz"
+    "https://github.com/cpp-pm/mxnet/archive/v1.5.0.rc1-b64e00a-p0.tar.gz"
     SHA1
     01c400835d280a7ffec86361d17a2a10b3851998
 )
