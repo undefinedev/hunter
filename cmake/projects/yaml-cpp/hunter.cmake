17e3c850384283b121c6538177727d19b44f8601
Rahul Sheth <rahul@snap.com>
2020-10-01 09:32:48 -0400

    Update project

--- cmake/projects/yaml-cpp/hunter.cmake
+++ cmake/projects/yaml-cpp/hunter.cmake
@@ -12,7 +12,7 @@ hunter_add_version(
   VERSION
   "0.5.3"
   URL
-  "https://github.com/hunter-packages/yaml-cpp/releases/download/release-0.5.3-hunter-p2/release-0.5.3-hunter-p2.tar.gz"
+  "https://github.com/cpp-pm/yaml-cpp/releases/download/release-0.5.3-hunter-p2/release-0.5.3-hunter-p2.tar.gz"
   SHA1
   ee1fe3713409de48b203f6b631c1284190d95671
   )
@@ -23,7 +23,7 @@ hunter_add_version(
   VERSION
   "0.5.3-plus-p0"
   URL
-  "https://github.com/hunter-packages/yaml-cpp/archive/release-0.5.3-plus-p0.zip"
+  "https://github.com/cpp-pm/yaml-cpp/archive/release-0.5.3-plus-p0.zip"
   SHA1
   3f612ceea5c983748368b6a543678f3d77e5ecc8
   )
@@ -34,7 +34,7 @@ hunter_add_version(
   VERSION
   "0.5.3-p4"
   URL
-  "https://github.com/hunter-packages/yaml-cpp/archive/v0.5.3-p4.zip"
+  "https://github.com/cpp-pm/yaml-cpp/archive/v0.5.3-p4.zip"
   SHA1
   b8de7495113c6462216ebb6b123f1b71ea761bc9
   )
@@ -45,7 +45,7 @@ hunter_add_version(
   VERSION
   "0.6.2-p0"
   URL
-  "https://github.com/hunter-packages/yaml-cpp/archive/v0.6.2-p0.zip"
+  "https://github.com/cpp-pm/yaml-cpp/archive/v0.6.2-p0.zip"
   SHA1
   d024c6ecd7b2466f134353d99e9a1e08eb9f95c4
   )
@@ -56,7 +56,7 @@ hunter_add_version(
   VERSION
   "0.6.2-0f9a586-p1"
   URL
-  "https://github.com/hunter-packages/yaml-cpp/archive/v0.6.2-0f9a586-p1.zip"
+  "https://github.com/cpp-pm/yaml-cpp/archive/v0.6.2-0f9a586-p1.zip"
   SHA1
   956c2b5fbf5aa0eb8ef5ef890c0328b3aa357a13
   )
