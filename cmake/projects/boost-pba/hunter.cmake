e250d2aa1203e31c8ddc92ec1930dd4fdc46e83b
Rahul Sheth <rahul@snap.com>
2020-10-01 09:31:23 -0400

    Update project

--- cmake/projects/boost-pba/hunter.cmake
+++ cmake/projects/boost-pba/hunter.cmake
@@ -16,7 +16,7 @@ hunter_add_version(
     VERSION
     "1.0.0"
     URL
-    "https://github.com/hunter-packages/boost-pba/archive/v1.0.0.tar.gz"
+    "https://github.com/cpp-pm/boost-pba/archive/v1.0.0.tar.gz"
     SHA1
     65b28b923cf0330860b36d568bf4a98503fb0455
 )
@@ -27,7 +27,7 @@ hunter_add_version(
     VERSION
     "1.0.0-p0"
     URL
-    "https://github.com/hunter-packages/boost-pba/archive/v1.0.0-p0.tar.gz"
+    "https://github.com/cpp-pm/boost-pba/archive/v1.0.0-p0.tar.gz"
     SHA1
     c4dd316edbb6839c5be2a5e1a9decd3bdfd848d0
 )
