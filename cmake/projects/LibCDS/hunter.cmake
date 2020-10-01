119d1be0b4bc1e1c4c200d4f301f3dc2e98d3246
Rahul Sheth <rahul@snap.com>
2020-10-01 09:31:53 -0400

    Update project

--- cmake/projects/LibCDS/hunter.cmake
+++ cmake/projects/LibCDS/hunter.cmake
@@ -8,7 +8,7 @@ include(hunter_pick_scheme)
 hunter_add_version(
     PACKAGE_NAME LibCDS
     VERSION "2.3.1"
-    URL "https://github.com/hunter-packages/libcds/archive/v2.3.1-p0.tar.gz"
+    URL "https://github.com/cpp-pm/libcds/archive/v2.3.1-p0.tar.gz"
     SHA1 "86cc0d2d57075830feaaa1c6e1200e970b96f9f8")
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
