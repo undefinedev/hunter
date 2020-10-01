610b69c9c6e7f21ab2948d627f78c6735e62fb11
Rahul Sheth <rahul@snap.com>
2020-10-01 09:32:36 -0400

    Update project

--- cmake/projects/tacopie/hunter.cmake
+++ cmake/projects/tacopie/hunter.cmake
@@ -15,7 +15,7 @@ hunter_add_version(
     VERSION
     "2.4.0-h1"
     URL
-    "https://github.com/hunter-packages/tacopie/archive/2.4.0-h1.tar.gz"
+    "https://github.com/cpp-pm/tacopie/archive/2.4.0-h1.tar.gz"
     SHA1
     5b326dd4e4792e63d9261682205f32944719bed0
 )
@@ -26,7 +26,7 @@ hunter_add_version(
     VERSION
     "3.2.0-h1"
     URL
-    "https://github.com/hunter-packages/tacopie/archive/3.2.0-h1.tar.gz"
+    "https://github.com/cpp-pm/tacopie/archive/3.2.0-h1.tar.gz"
     SHA1
     04a8953bf69f1fa26b68dd76df4b4baea291ab34
 )
