2d557b75b97bbc816e20b63a4c81132d06f2ab40
Rahul Sheth <rahul@snap.com>
2020-10-01 09:31:53 -0400

    Update project

--- cmake/projects/leveldb/hunter.cmake
+++ cmake/projects/leveldb/hunter.cmake
@@ -9,13 +9,13 @@ include(hunter_pick_scheme)
 hunter_add_version(
     PACKAGE_NAME leveldb
     VERSION "1.20"
-    URL "https://github.com/hunter-packages/leveldb/archive/v1.20-p3.tar.gz"
+    URL "https://github.com/cpp-pm/leveldb/archive/v1.20-p3.tar.gz"
     SHA1 "e843a3321605bd818628509c669d6dae65854657")
 
 hunter_add_version(
     PACKAGE_NAME leveldb
     VERSION "1.22"
-    URL "https://github.com/hunter-packages/leveldb/archive/v1.22-p0.tar.gz"
+    URL "https://github.com/cpp-pm/leveldb/archive/v1.22-p0.tar.gz"
     SHA1 "616e3816c0f44423136dcf286ae51417f0d75198")
 
 hunter_cmake_args(leveldb CMAKE_ARGS
