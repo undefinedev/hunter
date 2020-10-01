944c964b29d1a8c39785620b6a397fcd4300b44d
Rahul Sheth <rahul@snap.com>
2020-10-01 09:32:26 -0400

    Update project

--- cmake/projects/rang/hunter.cmake
+++ cmake/projects/rang/hunter.cmake
@@ -14,7 +14,7 @@ hunter_add_version(
     VERSION
     3.1.0-p0
     URL
-    "https://github.com/hunter-packages/rang/archive/v3.1.0-p0.tar.gz"
+    "https://github.com/cpp-pm/rang/archive/v3.1.0-p0.tar.gz"
     SHA1
     6a49f08c81009e2ab513b3d62e3c5fbc7efab882
 )
