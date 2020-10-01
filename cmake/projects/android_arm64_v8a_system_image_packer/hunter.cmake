bbc6a40387ebc92888b914397e5c558891ae853a
Rahul Sheth <rahul@snap.com>
2020-10-01 09:31:12 -0400

    Update project

--- cmake/projects/android_arm64_v8a_system_image_packer/hunter.cmake
+++ cmake/projects/android_arm64_v8a_system_image_packer/hunter.cmake
@@ -14,7 +14,7 @@ hunter_add_version(
     VERSION
     1.0.0
     URL
-    "https://github.com/hunter-packages/android_arm64_v8a_system_image_packer/archive/v1.0.0.tar.gz"
+    "https://github.com/cpp-pm/android_arm64_v8a_system_image_packer/archive/v1.0.0.tar.gz"
     SHA1
     e11d0b19f4d542c2a1347fb7d813a8efa9031622
 )
