759b8258171abd2854035e155b09d1d1acf12073
Rahul Sheth <rahul@snap.com>
2020-10-01 09:32:40 -0400

    Update project

--- cmake/projects/Urho3D/hunter.cmake
+++ cmake/projects/Urho3D/hunter.cmake
@@ -15,7 +15,7 @@ hunter_add_version(
     VERSION
     1.7-p2
     URL
-    "https://github.com/hunter-packages/Urho3D/archive/v1.7-p2.tar.gz"
+    "https://github.com/cpp-pm/Urho3D/archive/v1.7-p2.tar.gz"
     SHA1
     c828fad52e40475e34aa9829eb733dfe690b8942
 )
@@ -26,7 +26,7 @@ hunter_add_version(
     VERSION
     1.7-p4
     URL
-    "https://github.com/hunter-packages/Urho3D/archive/v1.7-p4.tar.gz"
+    "https://github.com/cpp-pm/Urho3D/archive/v1.7-p4.tar.gz"
     SHA1
     af0daeaa7be483ed98830b5080cfab54976db47e
 )
@@ -37,7 +37,7 @@ hunter_add_version(
     VERSION
     1.7-p5
     URL
-    "https://github.com/hunter-packages/Urho3D/archive/v1.7-p5.tar.gz"
+    "https://github.com/cpp-pm/Urho3D/archive/v1.7-p5.tar.gz"
     SHA1
     42bc93e302c8e35312fa966f4a9b1faf3ac8db7f
 )
@@ -48,7 +48,7 @@ hunter_add_version(
     VERSION
     1.7-p6
     URL
-    "https://github.com/hunter-packages/Urho3D/archive/v1.7-p6.tar.gz"
+    "https://github.com/cpp-pm/Urho3D/archive/v1.7-p6.tar.gz"
     SHA1
     b75bc0de6ff64c403f45a39d27d60a7cd6363038
 )
@@ -59,7 +59,7 @@ hunter_add_version(
     VERSION
     1.7-p7
     URL
-    "https://github.com/hunter-packages/Urho3D/archive/v1.7-p7.tar.gz"
+    "https://github.com/cpp-pm/Urho3D/archive/v1.7-p7.tar.gz"
     SHA1
     66b296e00edee7ba65c211a892e5ac0ae9a8877d
 )
@@ -70,7 +70,7 @@ hunter_add_version(
     VERSION
     1.7-p8
     URL
-    "https://github.com/hunter-packages/Urho3D/archive/v1.7-p8.tar.gz"
+    "https://github.com/cpp-pm/Urho3D/archive/v1.7-p8.tar.gz"
     SHA1
     c150a5495a317ad2d92187d31388d42a8d413434
 )
@@ -81,7 +81,7 @@ hunter_add_version(
     VERSION
     1.7-p9
     URL
-    "https://github.com/hunter-packages/Urho3D/archive/v1.7-p9.tar.gz"
+    "https://github.com/cpp-pm/Urho3D/archive/v1.7-p9.tar.gz"
     SHA1
     df696bba70384368a703c3650988e91951ce51f1
 )
@@ -92,7 +92,7 @@ hunter_add_version(
     VERSION
     1.7-p12
     URL
-    "https://github.com/hunter-packages/Urho3D/archive/v1.7-p12.tar.gz"
+    "https://github.com/cpp-pm/Urho3D/archive/v1.7-p12.tar.gz"
     SHA1
     1c6258cce46018d8386cf836bfda8fdfbfe225f6
 )
@@ -103,7 +103,7 @@ hunter_add_version(
     VERSION
     1.7-p13
     URL
-    "https://github.com/hunter-packages/Urho3D/archive/v1.7-p13.tar.gz"
+    "https://github.com/cpp-pm/Urho3D/archive/v1.7-p13.tar.gz"
     SHA1
     9dc5df770d7b454feb880ada76bcc0868879adfb
 )
@@ -114,7 +114,7 @@ hunter_add_version(
     VERSION
     1.7-p15
     URL
-    "https://github.com/hunter-packages/Urho3D/archive/v1.7-p15.tar.gz"
+    "https://github.com/cpp-pm/Urho3D/archive/v1.7-p15.tar.gz"
     SHA1
     2413d786c419845fdcb062c102f810503ddd09d4
 )
