1e5945e0b5af4491b29530452c6984b96f45a733
Rahul Sheth <rahul@snap.com>
2020-10-01 09:32:01 -0400

    Update project

--- cmake/projects/msgpack/hunter.cmake
+++ cmake/projects/msgpack/hunter.cmake
@@ -11,7 +11,7 @@ hunter_add_version(
   VERSION
   "1.4.1"
   URL
-  "https://github.com/hunter-packages/msgpack-c/archive/cpp-1.4.1-hunter-p1.tar.gz"
+  "https://github.com/cpp-pm/msgpack-c/archive/cpp-1.4.1-hunter-p1.tar.gz"
   SHA1
   5074d28857661693b71c4bda3f8fc662e4c73990
   )
@@ -22,7 +22,7 @@ hunter_add_version(
   VERSION
   "1.4.1-p2"
   URL
-  "https://github.com/hunter-packages/msgpack-c/archive/cpp-1.4.1-hunter-p2.tar.gz"
+  "https://github.com/cpp-pm/msgpack-c/archive/cpp-1.4.1-hunter-p2.tar.gz"
   SHA1
   eafdcb4c0a29d7416248740bd8132316a4b03910
   )
