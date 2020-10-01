fda4bbe411f47c1f46d3deb3304615b53470c703
Rahul Sheth <rahul@snap.com>
2020-10-01 09:31:14 -0400

    Update project

--- cmake/projects/android_sdk_tools_packer/hunter.cmake
+++ cmake/projects/android_sdk_tools_packer/hunter.cmake
@@ -14,7 +14,7 @@ hunter_add_version(
     VERSION
     1.0.0
     URL
-    "https://github.com/hunter-packages/android_sdk_tools_packer/archive/v1.0.0.tar.gz"
+    "https://github.com/cpp-pm/android_sdk_tools_packer/archive/v1.0.0.tar.gz"
     SHA1
     cff5f8931e74f3635b94282c7e0958fdd8cb740e
 )
@@ -25,7 +25,7 @@ hunter_add_version(
     VERSION
     1.0.1
     URL
-    "https://github.com/hunter-packages/android_sdk_tools_packer/archive/v1.0.1.tar.gz"
+    "https://github.com/cpp-pm/android_sdk_tools_packer/archive/v1.0.1.tar.gz"
     SHA1
     06f6788c558ee5891d07301208260fbc64545768
 )
@@ -36,7 +36,7 @@ hunter_add_version(
     VERSION
     1.0.2
     URL
-    "https://github.com/hunter-packages/android_sdk_tools_packer/archive/v1.0.2.tar.gz"
+    "https://github.com/cpp-pm/android_sdk_tools_packer/archive/v1.0.2.tar.gz"
     SHA1
     486140b9a3db1610aba7fcbda8474f2d97d40072
 )
@@ -47,7 +47,7 @@ hunter_add_version(
     VERSION
     1.0.3
     URL
-    "https://github.com/hunter-packages/android_sdk_tools_packer/archive/v1.0.3.tar.gz"
+    "https://github.com/cpp-pm/android_sdk_tools_packer/archive/v1.0.3.tar.gz"
     SHA1
     71a497152e3e1efe535df2c30210596e91042ecf
 )
