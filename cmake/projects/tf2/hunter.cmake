fd5256fd4e7401931265eb044e64252909ef14a3
Rahul Sheth <rahul@snap.com>
2020-10-01 09:32:37 -0400

    Update project

--- cmake/projects/tf2/hunter.cmake
+++ cmake/projects/tf2/hunter.cmake
@@ -15,7 +15,7 @@ hunter_add_version(
     VERSION
     0.6.5-p0
     URL
-    "https://github.com/hunter-packages/tf2/archive/v0.6.5-p0.tar.gz"
+    "https://github.com/cpp-pm/tf2/archive/v0.6.5-p0.tar.gz"
     SHA1
     ac8e28968074c6c386be8ecd2ac5ece29f5b0e06
 )
