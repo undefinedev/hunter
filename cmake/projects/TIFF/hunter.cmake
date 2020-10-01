e295c61f1529251516f8f5b186bbb46c14badbc0
Rahul Sheth <rahul@snap.com>
2020-10-01 09:32:38 -0400

    Update project

--- cmake/projects/TIFF/hunter.cmake
+++ cmake/projects/TIFF/hunter.cmake
@@ -14,7 +14,7 @@ hunter_add_version(
     VERSION
     "4.0.2-p3"
     URL
-    "https://github.com/hunter-packages/tiff/archive/v4.0.2-p3.tar.gz"
+    "https://github.com/cpp-pm/tiff/archive/v4.0.2-p3.tar.gz"
     SHA1
     f805dd2d7faa360e51a9e043d118b77d5b157bf1
 )
@@ -25,7 +25,7 @@ hunter_add_version(
     VERSION
     "4.0.2-hunter-2"
     URL
-    "https://github.com/hunter-packages/tiff/archive/v4.0.2-hunter-2.tar.gz"
+    "https://github.com/cpp-pm/tiff/archive/v4.0.2-hunter-2.tar.gz"
     SHA1
     a891cb185256925d983e87d664304c4b6ff31779
 )
@@ -36,7 +36,7 @@ hunter_add_version(
     VERSION
     "4.0.2-hunter-1"
     URL
-    "https://github.com/hunter-packages/tiff/archive/v4.0.2-hunter-1.tar.gz"
+    "https://github.com/cpp-pm/tiff/archive/v4.0.2-hunter-1.tar.gz"
     SHA1
     9088be15b54257f227988e1b479b7394e944fe71
 )
@@ -47,7 +47,7 @@ hunter_add_version(
     VERSION
     "4.0.2"
     URL
-    "https://github.com/hunter-packages/tiff/archive/v4.0.2.tar.gz"
+    "https://github.com/cpp-pm/tiff/archive/v4.0.2.tar.gz"
     SHA1
     37c71656488797c4e5fde620570f1a1b9be36037
 )
@@ -58,7 +58,7 @@ hunter_add_version(
     VERSION
     "4.0.2-p4"
     URL
-    "https://github.com/hunter-packages/tiff/archive/v4.0.2-p4.tar.gz"
+    "https://github.com/cpp-pm/tiff/archive/v4.0.2-p4.tar.gz"
     SHA1
     9ab44e0136d3da04b0a559d7ba57bd075afc48f9
 )
@@ -69,7 +69,7 @@ hunter_add_version(
     VERSION
     "4.0.2-p5"
     URL
-    "https://github.com/hunter-packages/tiff/archive/v4.0.2-p5.tar.gz"
+    "https://github.com/cpp-pm/tiff/archive/v4.0.2-p5.tar.gz"
     SHA1
     7bee2843b47c5f9865973b7235e58aa3fb26e1b0
 )
