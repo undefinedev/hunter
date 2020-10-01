e7142d44437cbcd95a8a7d200ded6ae121804a20
Rahul Sheth <rahul@snap.com>
2020-10-01 09:32:08 -0400

    Update project

--- cmake/projects/pcg/hunter.cmake
+++ cmake/projects/pcg/hunter.cmake
@@ -9,7 +9,7 @@ include(hunter_pick_scheme)
 hunter_add_version(
     PACKAGE_NAME pcg
     VERSION "0.0.0-p1"
-    URL "https://github.com/hunter-packages/pcg-c/archive/v0.0.0-p1.tar.gz"
+    URL "https://github.com/cpp-pm/pcg-c/archive/v0.0.0-p1.tar.gz"
     SHA1 "5036df47f4b4c62e6a2e267abc1ca2f7a657d62c")
 
 hunter_cmake_args(pcg CMAKE_ARGS BUILD_TESTING=OFF)
