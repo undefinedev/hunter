d86107ab4def994b76b1c5a17fad6550eb2c217a
Rahul Sheth <rahul@snap.com>
2020-10-01 09:32:40 -0400

    Update project

--- cmake/projects/type_safe/hunter.cmake
+++ cmake/projects/type_safe/hunter.cmake
@@ -15,7 +15,7 @@ hunter_add_version(
     VERSION
     "0.1"
     URL
-    "https://github.com/hunter-packages/type_safe/archive/v0.1-p.tar.gz"
+    "https://github.com/cpp-pm/type_safe/archive/v0.1-p.tar.gz"
     SHA1
     57e426af70791485e2a209c602ce9d71e9d52ab7
 )
@@ -26,7 +26,7 @@ hunter_add_version(
     VERSION
     "0.1-p2"
     URL
-    "https://github.com/hunter-packages/type_safe/archive/0.1-p2.tar.gz"
+    "https://github.com/cpp-pm/type_safe/archive/0.1-p2.tar.gz"
     SHA1
     6a1fa6f3926a5d0175890647ff0f419ba4c562f4
 )
@@ -37,7 +37,7 @@ hunter_add_version(
     VERSION
     "0.2"
     URL
-    "https://github.com/hunter-packages/type_safe/archive/v0.2-p.tar.gz"
+    "https://github.com/cpp-pm/type_safe/archive/v0.2-p.tar.gz"
     SHA1
     7f8ba49a04f04d7f121ec358521a5d7a8f5039dd
 )
@@ -48,7 +48,7 @@ hunter_add_version(
     VERSION
     "0.2-p1"
     URL
-    "https://github.com/hunter-packages/type_safe/archive/v0.2-p1.tar.gz"
+    "https://github.com/cpp-pm/type_safe/archive/v0.2-p1.tar.gz"
     SHA1
     d051bec1db05d0623b6e8a6fd2805e70ba914713
 )
@@ -59,7 +59,7 @@ hunter_add_version(
     VERSION
     "0.2.1-p1"
     URL
-    "https://github.com/hunter-packages/type_safe/archive/v0.2.1-p1.zip"
+    "https://github.com/cpp-pm/type_safe/archive/v0.2.1-p1.zip"
     SHA1
     6d583f877cce781ff472f1a0a2d56af68afa5ff1
 )
@@ -70,7 +70,7 @@ hunter_add_version(
     VERSION
     "0.2.1-p2"
     URL
-    "https://github.com/hunter-packages/type_safe/archive/v0.2.1-p2.zip"
+    "https://github.com/cpp-pm/type_safe/archive/v0.2.1-p2.zip"
     SHA1
     09a76b0668cd7bf6c6b590c80e64cb2745a32bb9
 )
