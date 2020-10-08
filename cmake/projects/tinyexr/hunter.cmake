b8360cd5e2732fe5c3179c11854b6dd7b1036fdd
Rahul Sheth <rahul@snap.com>
2020-10-08 17:57:44 -0400

    Add 'tinyexr' package (#278)

---
+++ cmake/projects/tinyexr/hunter.cmake
@@ -0,0 +1,24 @@
+# Copyright (c) 2016-2020, Rahul Sheth, Ruslan Baratov
+# All rights reserved.
+
+# !!! DO NOT PLACE HEADER GUARDS HERE !!!
+
+include(hunter_add_version)
+include(hunter_cacheable)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+hunter_add_version(
+    PACKAGE_NAME
+    tinyexr
+    VERSION
+    1.0.0-297e645-p0
+    URL
+    "https://github.com/cpp-pm/tinyexr/archive/v1.0.0-297e645-p0.tar.gz"
+    SHA1
+    7d17e84ddd7ba48b11ab27104bcb92222bb2ef9f
+)
+
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+hunter_cacheable(tinyexr)
+hunter_download(PACKAGE_NAME tinyexr)
