181f4d921c3acea82ee3f6e431bc664e5ccd89c5
elisemorysc <esemory@snapchat.com>
2020-11-02 11:38:21 -0500

    Add SPIRV-Headers 1.5.1.corrected (#297)
    * Add SPIRV-Headers 1.5.1.corrected
    
    * remove hunter config
    
    Co-authored-by: Eli Semory <elisemory@snap.com>

--- cmake/projects/SPIRV-Headers/hunter.cmake
+++ cmake/projects/SPIRV-Headers/hunter.cmake
@@ -25,6 +25,17 @@ hunter_add_version(
     PACKAGE_NAME
     SPIRV-Headers
     VERSION
+    1.5.1.corrected
+    URL
+    https://github.com/KhronosGroup/SPIRV-Headers/archive/1.5.1.corrected.tar.gz
+    SHA1
+    e7911f27a13c61e9d8c13fe7b0df5f1a883e972e
+)
+
+hunter_add_version(
+    PACKAGE_NAME
+    SPIRV-Headers
+    VERSION
     1.5.3.reservations1
     URL
     https://github.com/KhronosGroup/SPIRV-Headers/archive/1.5.3.reservations1.tar.gz
