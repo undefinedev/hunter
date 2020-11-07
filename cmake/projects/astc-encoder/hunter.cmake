48e2033c56e025c1aa118c9403756457ed01e42a
Rahul Sheth <rahul@snap.com>
2020-11-06 22:23:48 -0500

    Add astc-encoder v1.3-a47b80f-p1 (#302)
    * Update astc-encoder to 1.3-a47b80f-p1
    
    * Update example
    
    * Update SHA1
    
    * Revert testing changes

--- cmake/projects/astc-encoder/hunter.cmake
+++ cmake/projects/astc-encoder/hunter.cmake
@@ -31,6 +31,17 @@ hunter_add_version(
     6a181bffbacec7a6c2ee99a0c9bdb9da3de09c95
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    astc-encoder
+    VERSION
+    1.3-a47b80f-p1
+    URL
+    "https://github.com/cpp-pm/astc-encoder/archive/v1.3-a47b80f-p1.tar.gz"
+    SHA1
+    c01b9663ffd6e6a80cca985d24c4e77fcbaa65e7
+)
+
 if(ANDROID OR IOS)
   hunter_cmake_args(
     astc-encoder
--- examples/astc-encoder/boo.cpp
+++ examples/astc-encoder/boo.cpp
@@ -1,5 +1,6 @@
 #include <astc-encoder/astcenc.h>
 
 int main() {
-    astcenc_config my_config;
+    astcenc_config my_config; // for v2.0+
+    // astc_codec_image image; // for v1.3
 }
