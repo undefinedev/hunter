7b36a9e0419793e6099a0968b02f03a97d0162e8
Norbert Wenzel <norbertwenzel@users.noreply.github.com>
2020-12-21 20:47:13 +0100

    Update 'termcolor' to v1.0.1 (#329)

--- cmake/projects/termcolor/hunter.cmake
+++ cmake/projects/termcolor/hunter.cmake
@@ -19,6 +19,17 @@ hunter_add_version(
     3ecab2daf39f118bff2244f51760dfd32ab7a3d1
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    termcolor
+    VERSION
+    "1.0.1"
+    URL
+    "https://github.com/ikalnytskyi/termcolor/archive/v1.0.1.tar.gz"
+    SHA1
+    c49b9d07c3240ba624e6471a5e461ecaa2fc7dc2
+)
+
 hunter_cmake_args(termcolor CMAKE_ARGS ENABLE_TESTING=OFF)
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
