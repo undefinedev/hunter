7cb605a25ebdef502d851b6d00431034a84bd8ca
Rahul Sheth <rahul@snap.com>
2021-01-29 16:19:23 -0500

    Update draco to v1.4.1-p0 (#355)
    * Update draco to v1.4.1-p0
    
    * Update SHA1
    
    * Update SHA1

--- cmake/projects/draco/hunter.cmake
+++ cmake/projects/draco/hunter.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) 2016-2018, Ruslan Baratov
+# Copyright (c) 2016-2021, Ruslan Baratov, Rahul Sheth
 # All rights reserved.
 
 # !!! DO NOT PLACE HEADER GUARDS HERE !!!
@@ -17,6 +17,17 @@ hunter_add_version(
     "https://github.com/hunter-packages/draco/archive/v1.3.4-p0.tar.gz"
     SHA1
     64cd8ac43fc213ad4cb088aa7bba190adb87bb5d
+)
+
+hunter_add_version(
+    PACKAGE_NAME
+    draco
+    VERSION
+    1.4.1-p0
+    URL
+    "https://github.com/cpp-pm/draco/archive/v1.4.1-p0.tar.gz"
+    SHA1
+    1c37aba7278d7b8c729a98cc429d867626fa085c
 )
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
