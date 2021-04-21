d38471381f07dd9487ee0d423f99edfe385fe0eb
Rahul Sheth <rahul@snap.com>
2021-04-21 12:56:37 -0400

    Update aws-c-common to v0.5.6 (#389)

--- cmake/projects/aws-c-common/hunter.cmake
+++ cmake/projects/aws-c-common/hunter.cmake
@@ -19,6 +19,17 @@ hunter_add_version(
     65cf17a47d77054a4c043ef9c59978c872f8ed3d
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    aws-c-common
+    VERSION
+    0.5.6
+    URL
+    "https://github.com/awslabs/aws-c-common/archive/refs/tags/v0.5.6.tar.gz"
+    SHA1
+    a8abc1a41e78da167f2760ab538272d1014fd04c
+)
+
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_cacheable(aws-c-common)
 hunter_download(PACKAGE_NAME aws-c-common)
