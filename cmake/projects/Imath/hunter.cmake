1e0725e8ebdd2d3b87107c2b44911e5f2cd32f31
Harry Mallon <hjmallon@gmail.com>
2021-05-03 15:20:11 +0100

    Add 'Imath' package (#391)

---
+++ cmake/projects/Imath/hunter.cmake
@@ -0,0 +1,31 @@
+# Copyright (c) 2016-2020, Rahul Sheth, Ruslan Baratov
+# All rights reserved.
+
+# !!! DO NOT PLACE HEADER GUARDS HERE !!!
+
+include(hunter_add_version)
+include(hunter_cacheable)
+include(hunter_cmake_args)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+hunter_add_version(
+    PACKAGE_NAME
+    Imath
+    VERSION
+    3.0.1
+    URL
+    "https://github.com/AcademySoftwareFoundation/Imath/archive/refs/tags/v3.0.1.tar.gz"
+    SHA1
+    967271b4b04b62014bd321958d73c77c22e60eb6
+)
+
+hunter_cmake_args(
+    Imath
+    CMAKE_ARGS
+        BUILD_TESTING=OFF
+)
+
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+hunter_cacheable(Imath)
+hunter_download(PACKAGE_NAME Imath)
