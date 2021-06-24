75e40420545969f64bc412314d23d13766ee51a7
Rahul Sheth <rahul@snap.com>
2021-06-24 15:20:26 -0400

    Update KhronosDataFormat to 1.3.1-1f8c852-p2 (#413)

--- cmake/projects/KhronosDataFormat/hunter.cmake
+++ cmake/projects/KhronosDataFormat/hunter.cmake
@@ -30,6 +30,17 @@ hunter_add_version(
     41dd36ba55bd5c4defbd6fea5e0cffd9c81d62a5
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    KhronosDataFormat
+    VERSION
+    1.3.1-1f8c852-p2
+    URL
+    "https://github.com/cpp-pm/DataFormat/archive/v1.3.1-1f8c852-p2.tar.gz"
+    SHA1
+    18f38cbeb1644834ba7b3b464f7d1858446c19b1
+)
+
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_cacheable(KhronosDataFormat)
 hunter_download(PACKAGE_NAME KhronosDataFormat)
