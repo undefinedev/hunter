7c743ca598212659f38b1d09d3c8123addd3463e
Rahul Sheth <rahul@snap.com>
2021-07-06 15:44:58 +0000

    Update astc-encoder to v3.0-7257cbd-p0 (#426)

--- cmake/projects/astc-encoder/hunter.cmake
+++ cmake/projects/astc-encoder/hunter.cmake
@@ -13,17 +13,6 @@ hunter_add_version(
     PACKAGE_NAME
     astc-encoder
     VERSION
-    2.0-ab16aaa-p0
-    URL
-    "https://github.com/cpp-pm/astc-encoder/archive/v2.0-ab16aaa-p0.tar.gz"
-    SHA1
-    027d46d8c190cbfc3c7ab0b78a7773e534f9deec
-)
-
-hunter_add_version(
-    PACKAGE_NAME
-    astc-encoder
-    VERSION
     1.3-a47b80f-p0
     URL
     "https://github.com/cpp-pm/astc-encoder/archive/v1.3-a47b80f-p0.tar.gz"
@@ -42,20 +31,60 @@ hunter_add_version(
     c01b9663ffd6e6a80cca985d24c4e77fcbaa65e7
 )
 
-if(ANDROID OR IOS)
-  hunter_cmake_args(
+hunter_add_version(
+    PACKAGE_NAME
     astc-encoder
-    CMAKE_ARGS
-      VEC=neon
-      BUILD_APPS=OFF
-  )
-else()
-  hunter_cmake_args(
+    VERSION
+    2.0-ab16aaa-p0
+    URL
+    "https://github.com/cpp-pm/astc-encoder/archive/v2.0-ab16aaa-p0.tar.gz"
+    SHA1
+    027d46d8c190cbfc3c7ab0b78a7773e534f9deec
+)
+
+hunter_add_version(
+    PACKAGE_NAME
     astc-encoder
-    CMAKE_ARGS
-      VEC=sse2
-      BUILD_APPS=OFF
-  )
+    VERSION
+    3.0-7257cbd-p0
+    URL
+    "https://github.com/cpp-pm/astc-encoder/archive/refs/tags/v3.0-7257cbd-p0.tar.gz"
+    SHA1
+    8cae9efba722ff7effc3f08f2ec86cd5676dee91
+)
+
+if(HUNTER_astc-encoder_VERSION VERSION_LESS 3.0)
+  if(ANDROID OR IOS)
+    hunter_cmake_args(
+      astc-encoder
+      CMAKE_ARGS
+        VEC=neon
+        BUILD_APPS=OFF
+    )
+  else()
+    hunter_cmake_args(
+      astc-encoder
+      CMAKE_ARGS
+        VEC=sse2
+        BUILD_APPS=OFF
+    )
+  endif()
+else() # 3.0 and over
+  if(ANDROID OR IOS)
+    hunter_cmake_args(
+      astc-encoder
+      CMAKE_ARGS
+        ISA_NEON=ON
+        CLI=OFF
+    )
+  else()
+    hunter_cmake_args(
+      astc-encoder
+      CMAKE_ARGS
+        ISA_SSE2=ON
+        CLI=OFF
+    )
+  endif()
 endif()
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
--- examples/astc-encoder/boo.cpp
+++ examples/astc-encoder/boo.cpp
@@ -1,4 +1,5 @@
-#include <astc-encoder/astcenc.h>
+// #include <astc-encoder/astcenc.h> // for v1.0-v2.0
+#include <astcenc/astcenc.h> // for v3.0+
 
 int main() {
     astcenc_config my_config; // for v2.0+
