98255524a353dc11fd05de117f0b7300b7a9e306
Rahul Sheth <rahul@snap.com>
2021-07-26 13:01:24 -0400

    Add some utility scripts

--- maintenance/create_package.sh
+++ maintenance/create_package.sh
@@ -1,35 +1,24 @@
 #!/bin/bash
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 PACKAGE_NAME=$1
 PACKAGE_VERSION=$2
 PACKAGE_URL=$3
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    SED_EXECUTABLE=gsed
-    if command -v $SED_EXECUTABLE > /dev/null 2>&1; then
-        echo "Found gsed"
-    else
-        echo "GNU sed not installed. Try brew install gsed."
-        exit 1
-    fi
-else
-    SED_EXECUTABLE=sed
-fi
-
 echo "Creating package files for $PACKAGE_NAME"
 
-winpty wget $3 -O package.tar.gz
-PACKAGE_SHA1=$(openssl sha1 package.tar.gz | $SED_EXECUTABLE 's/^.* //')
-rm package.tar.gz
+PACKAGE_SHA1=$($SCRIPT_DIR/get_package_sha1.sh $3)
+SED_COMMAND=$($SCRIPT_DIR/get_sed_command.sh)
 
 cp -r cmake/projects/foo cmake/projects/$PACKAGE_NAME
-$SED_EXECUTABLE -i "s,foo,$PACKAGE_NAME,g" cmake/projects/$PACKAGE_NAME/hunter.cmake
-$SED_EXECUTABLE -i "s,\"https.*\",\"$PACKAGE_URL\",g" cmake/projects/$PACKAGE_NAME/hunter.cmake
-$SED_EXECUTABLE -i "s,[1]\{40\},$PACKAGE_SHA1,g" cmake/projects/$PACKAGE_NAME/hunter.cmake
-$SED_EXECUTABLE -i "s,1\.0\.0,$PACKAGE_VERSION,g" cmake/projects/$PACKAGE_NAME/hunter.cmake
+$SED_COMMAND -i "s,foo,$PACKAGE_NAME,g" cmake/projects/$PACKAGE_NAME/hunter.cmake
+$SED_COMMAND -i "s,\"https.*\",\"$PACKAGE_URL\",g" cmake/projects/$PACKAGE_NAME/hunter.cmake
+$SED_COMMAND -i "s,[1]\{40\},$PACKAGE_SHA1,g" cmake/projects/$PACKAGE_NAME/hunter.cmake
+$SED_COMMAND -i "s,1\.0\.0,$PACKAGE_VERSION,g" cmake/projects/$PACKAGE_NAME/hunter.cmake
 
 cp -r examples/foo examples/$PACKAGE_NAME
-$SED_EXECUTABLE -i "s,foo,$PACKAGE_NAME,g" examples/$PACKAGE_NAME/*
+$SED_COMMAND -i "s,foo,$PACKAGE_NAME,g" examples/$PACKAGE_NAME/*
 
 cp -r docs/packages/pkg/foo.rst docs/packages/pkg/$PACKAGE_NAME.rst
-$SED_EXECUTABLE -i "s,foo,$PACKAGE_NAME,g" docs/packages/pkg/$PACKAGE_NAME.rst
+$SED_COMMAND -i "s,foo,$PACKAGE_NAME,g" docs/packages/pkg/$PACKAGE_NAME.rst
