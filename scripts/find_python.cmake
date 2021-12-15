6171c320dceed3e4ee8651db50d68633cd9dc48f
TheMarpe <martin.peterlin7@gmail.com>
2021-12-15 23:41:17 +0100

    Reduced required CMake version (#498)

--- scripts/find_python.cmake
+++ scripts/find_python.cmake
@@ -1,14 +1,24 @@
-cmake_minimum_required(VERSION 3.12)
+cmake_minimum_required(VERSION 3.0)
 
-if(DEFINED ENV{HUNTER_PYTHON_LOCATION})
-  set(Python_ROOT_DIR $ENV{HUNTER_PYTHON_LOCATION})
-  set(Python_FIND_STRATEGY LOCATION)
+if(${CMAKE_VERSION} VERSION_LESS "3.12.0")
+  find_package(PythonInterp 3 QUIET)
+  if(NOT PYTHONINTERP_FOUND)
+    message(FATAL_ERROR "Python not found")
+  endif()
+  message(${PYTHON_EXECUTABLE})
+else()
+  cmake_minimum_required(VERSION 3.12)
+
+  if(DEFINED ENV{HUNTER_PYTHON_LOCATION})
+    set(Python_ROOT_DIR $ENV{HUNTER_PYTHON_LOCATION})
+    set(Python_FIND_STRATEGY LOCATION)
+  endif()
+
+  find_package(Python COMPONENTS Interpreter QUIET)
+
+  if(NOT Python_Interpreter_FOUND)
+    message(FATAL_ERROR "Python not found")
+  endif()
+
+  message(${Python_EXECUTABLE})
 endif()
-
-find_package(Python COMPONENTS Interpreter QUIET)
-
-if(NOT Python_Interpreter_FOUND)
-  message(FATAL_ERROR "Python not found")
-endif()
-
-message(${Python_EXECUTABLE})
