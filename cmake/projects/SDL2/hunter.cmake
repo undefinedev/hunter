e2424a4457a1fcb406e78500571e958030384fa9
drodin <rodin.dmitry@gmail.com>
2021-12-21 01:45:08 +0700

    Update 'SDL2' package to v2.0.18 (#502)

--- cmake/projects/SDL2/hunter.cmake
+++ cmake/projects/SDL2/hunter.cmake
@@ -153,6 +153,17 @@ hunter_add_version(
         PACKAGE_NAME
         SDL2
         VERSION
+        "2.0.18"
+        URL
+        "https://github.com/libsdl-org/SDL/archive/release-2.0.18.tar.gz"
+        SHA1
+        b765ac6a7bfc617ce51ff2571b1ae056357413f2
+)
+
+hunter_add_version(
+        PACKAGE_NAME
+        SDL2
+        VERSION
         "2.0.4-urho-p2"
         URL
         "https://github.com/cpp-pm/SDL2_legacy/archive/v2.0.4-urho-p2.tar.gz"
