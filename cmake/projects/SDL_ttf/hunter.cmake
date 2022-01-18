87144f9349c972ba0a8ce16b09ac8e33754ac38a
drodin <rodin.dmitry@gmail.com>
2022-01-19 01:11:53 +0700

    Update 'SDL_ttf' package to v2.0.18-p0 (#510)

--- cmake/projects/SDL_ttf/hunter.cmake
+++ cmake/projects/SDL_ttf/hunter.cmake
@@ -28,6 +28,17 @@ hunter_add_version(
     be9de6d9eb411e9e0910a26a13cd29d234e51906
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    SDL_ttf
+    VERSION
+    "2.0.18-p0"
+    URL
+    "https://github.com/cpp-pm/SDL_ttf/archive/v2.0.18-p0.tar.gz"
+    SHA1
+    051ef15f3037002bd26414af56529e3bbb89a044
+)
+
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_cacheable(SDL_ttf)
 hunter_download(PACKAGE_NAME SDL_ttf)
