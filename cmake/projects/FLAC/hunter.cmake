dd4172a5631fa0d9f3e329455393091da9a51047
drodin <rodin.dmitry@gmail.com>
2022-02-24 01:43:17 +0700

    Update 'FLAC' package to v1.3.4-p0 (#523)

--- cmake/projects/FLAC/hunter.cmake
+++ cmake/projects/FLAC/hunter.cmake
@@ -15,6 +15,17 @@ hunter_add_version(
     712ff57b046ed00ebc2898989290c148cfda5c36
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    FLAC
+    VERSION
+    1.3.4-p0
+    URL
+    "https://github.com/cpp-pm/flac/archive/v1.3.4-p0.tar.gz"
+    SHA1
+    ec51d29efd8fc29a873c50c0185ff733f532a656
+)
+
 set(_hunter_flac_cmake_args)
 if(ANDROID OR IOS)
     set(
@@ -27,10 +38,14 @@ hunter_cmake_args(
     FLAC
     CMAKE_ARGS
         WITH_OGG=OFF
+        BUILD_PROGRAMS=OFF
         BUILD_EXECUTABLES=OFF
         BUILD_EXAMPLES=OFF
+        BUILD_DOCS=OFF
         BUILD_TESTING=OFF
         BUILD_DOXYGEN=OFF
+        INSTALL_MANPAGES=OFF
+        INSTALL_PKGCONFIG_MODULES=OFF
         ${_hunter_flac_cmake_args}
 )
 
